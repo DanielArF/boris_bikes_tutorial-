b0c3729caee3956cd63878ca0e71016ad13158a6
DanielArF <daniel_af96@hotmail.com>
2015-09-14 22:57:35 +0200

    add a working test for release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,5 +2,13 @@ require 'docking_station'
 
 describe DockingStation do 
 
+	subject { DockingStation.new }
 
+	it 'can release a bike' do 
+		expect(subject).to respond_to :release_bike 
+		#subject.should respond_to release_bike 
+	end 
+
+	#it { is_expected.to respond_to :release_bike } 
+	
 end 
