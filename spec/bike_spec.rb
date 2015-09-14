13d5fc55e6339a39a2e137bb210cfd6e84fdcbc4
DanielArF <daniel_af96@hotmail.com>
2015-09-14 23:24:35 +0200

    build a bike

---
+++ spec/bike_spec.rb
@@ -0,0 +1,12 @@
+require 'bike'
+
+describe Bike do
+
+	subject { Bike.new }
+	
+	it { is_expected.to respond_to :working? }
+
+
+
+
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,11 +4,11 @@ describe DockingStation do
 
 	subject { DockingStation.new }
 
-	it 'can release a bike' do 
-		expect(subject).to respond_to :release_bike 
-		#subject.should respond_to release_bike 
-	end 
+	# it 'can release a bike' do 
+	# 	expect(subject).to respond_to :release_bike 
+	# 	#subject.should respond_to release_bike 
+	# end 
 
-	#it { is_expected.to respond_to :release_bike } 
-	
+	it { is_expected.to respond_to :release_bike } 
+
 end 
